dafc0fd3c58edd76c6c961f82a91dfe9aff19637
madchick <madchick@gmail.com>
2020-06-05 21:19:41 +0900

    Initial commit

---
+++ src/com/madchick/springStudy/MyApp.java
@@ -0,0 +1,10 @@
+package com.madchick.springStudy;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+	}
+}
